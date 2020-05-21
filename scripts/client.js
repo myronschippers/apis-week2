59b4d4be8666f41caf26f5f08310e9166dd60c3e
Myron <dubtwee@gmail.com>
2020-05-21 18:37:08 -0500

    evening lecture notes

--- scripts/client.js
+++ scripts/client.js
@@ -1,55 +1,61 @@
 console.log('Hello Apis!!!');
 
+// declare a variable
 // const
 // let
 
-let kittyKat = 'Fluffy'; // allows reassigning variable value
-const doggy = 'value'; // value cannot be reassigned
+const doggy = 'Bowser';
+console.log('doggy:', doggy);
+let kittyKat = 'Fluffy';
 
-kittyKat = 'Lil Mr';
 console.log('kittyKat:', kittyKat);
 
-// number 0 1 2 3 3000
-kittyKat = 3;
+// string '', ' ', 'Apis', '3', 'ture'
+kittyKat = 'Spike';
 console.log('kittyKat:', kittyKat);
 
-// string ' ' 'dog' 'Kat'
-kittyKat = '5';
-console.log('kittyKat:', kittyKat);
-
-// boolean true false
+// boolean = true or false
 kittyKat = false;
+console.log('kittyKat:', kittyKat);
+
+// number 0, 1, 2, 3000
+kittyKat = 3;
 console.log('kittyKat:', kittyKat);
 
 
 const basketMaxFruit = 6;
-const fruitPurchased = '6';
+const fruitForPurchase = 6;
 
-if (fruitPurchased < basketMaxFruit) {
+if (fruitForPurchase < basketMaxFruit) {
   console.log('You can carry more fruit.');
-} else if (fruitPurchased === basketMaxFruit) {
+} else if (fruitForPurchase === basketMaxFruit) {
   console.log('Your basket is full.');
 } else {
   console.log('You have too much fruit.');
 }
 
-if (!kittyKat) {
-  console.log('Kitty is a false');
-}
-
 // < less than
 // > greater than
 // <= less than or equal to
-// >= greater than or equal to
+// >= greater than or euqal to
 // == evaluates value only
-// === evaluates value and type
-// != does not equal - only value
-// !== does not eauql - value and type
+// === evaluate both value and type
+// != does not equal evaluate value only
+// !== does not equal evalutaing value and type
 // ! checking for falsy value
 
 let isSnowing;
+console.log('isSnowing: ', isSnowing);
+isSnowing = '';
+console.log('isSnowing: ', isSnowing);
+isSnowing = false;
+console.log('isSnowing: ', isSnowing);
+isSnowing = 0;
+console.log('isSnowing: ', isSnowing);
+isSnowing = null;
+console.log('isSnowing: ', isSnowing);
 
-// falsy values are: false, '', null, undefined, 0
+// falsy values: undefined, '', false, 0, null
 if (!isSnowing) {
-  console.log('Hurray it\'s not snowing');
+  console.log('Yay!!!!! It\'s not snowing.');
 }
